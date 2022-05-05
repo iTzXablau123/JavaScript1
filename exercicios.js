12a83a2d5ccafeb9b8ba59a77906815425f08532
Xablau <102757880+iTzXablau123@users.noreply.github.com>
2022-05-05 13:32:38 -0300

    Add files via upload

---
+++ exercicios.js
@@ -0,0 +1,30 @@
+//1
+//A)
+console.log(10+ 20 * 30);
+//B)
+console.log( 4**2/30); 
+//C)
+console.log(9**4)*(6-1);
+
+//2.
+//a)
+console.log (10% 3 * 10 ** 1 + 1 -10 *4 / 2);
+
+//3
+const nome = 'Kevin Menezes Rocha';
+console.log (nome);
+
+//4
+const x = 3;
+const y = 5;
+console.log (2*x*3*y);
+
+//5 
+const a = 10;
+const b = 40;
+const c = 24;
+console.log (a+b+c);
+
+//6
+const valor = 1800;
+console.log (valor*2);
